68f46beabf59e180bc9ed062da67ec42a9a15cf0
IceFuchs <icefuchshd@gmail.com>
2019-07-12 23:36:14 +0200

    update

--- src/me/depeloper/openapi/OpenAPI.java
+++ src/me/depeloper/openapi/OpenAPI.java
@@ -1,4 +1,44 @@
 package me.depeloper.openapi;
 
-public class OpenAPI {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OpenAPI extends JavaPlugin {
+
+    private static OpenAPI instance;
+
+    public void onEnable()
+    {
+        System.out.println("\n" +
+                " #######  ########  ######## ##    ##    ###    ########  #### \n" +
+                "##     ## ##     ## ##       ###   ##   ## ##   ##     ##  ##  \n" +
+                "##     ## ##     ## ##       ####  ##  ##   ##  ##     ##  ##  \n" +
+                "##     ## ########  ######   ## ## ## ##     ## ########   ##  \n" +
+                "##     ## ##        ##       ##  #### ######### ##         ##  \n" +
+                "##     ## ##        ##       ##   ### ##     ## ##         ##  \n" +
+                " #######  ##        ######## ##    ## ##     ## ##        #### \n");
+
+        this.init();
+    }
+
+    public void onDisable()
+    {
+        System.out.println("\n" +
+                " #######  ########  ######## ##    ##    ###    ########  #### \n" +
+                "##     ## ##     ## ##       ###   ##   ## ##   ##     ##  ##  \n" +
+                "##     ## ##     ## ##       ####  ##  ##   ##  ##     ##  ##  \n" +
+                "##     ## ########  ######   ## ## ## ##     ## ########   ##  \n" +
+                "##     ## ##        ##       ##  #### ######### ##         ##  \n" +
+                "##     ## ##        ##       ##   ### ##     ## ##         ##  \n" +
+                " #######  ##        ######## ##    ## ##     ## ##        #### \n");
+    }
+
+    private void init()
+    {
+        instance = this;
+    }
+
+    public static OpenAPI getInstance()
+    {
+        return instance;
+    }
 }
