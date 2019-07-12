7b7162773435880b65467ce325c76d8e82c51523
IceFuchs <icefuchshd@gmail.com>
2019-07-12 23:50:39 +0200

    update

--- src/me/depeloper/openapi/command/SimpleCommand.java
+++ src/me/depeloper/openapi/command/SimpleCommand.java
@@ -1,4 +1,39 @@
 package me.depeloper.openapi.command;
 
-public class SimpleCommand {
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+
+import java.lang.reflect.Field;
+
+public abstract class SimpleCommand extends BukkitCommand {
+
+    public SimpleCommand(String name)
+    {
+        super(name);
+
+        getCommandMap().register(name, this);
+    }
+
+    public boolean execute(CommandSender commandSender, String s, String[] strings)
+    {
+        return on(commandSender, strings);
+    }
+
+    public abstract boolean on(CommandSender sender, String[] args);
+
+    private CommandMap getCommandMap()
+    {
+        try
+        {
+            Field field = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            field.setAccessible(true);
+            return (CommandMap) field.get("commandMap");
+        } catch (NoSuchFieldException | IllegalAccessException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/me/depeloper/openapi/event/SimpleEvent.java
+++ src/me/depeloper/openapi/event/SimpleEvent.java
@@ -1,4 +1,24 @@
 package me.depeloper.openapi.event;
 
-public class SimpleEvent {
+import me.depeloper.openapi.OpenAPI;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+public abstract class SimpleEvent<E extends Event> implements Listener {
+
+    private final OpenAPI plugin;
+
+    public SimpleEvent()
+    {
+        this.plugin = OpenAPI.getInstance();
+        this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    public abstract void on(E event);
+
+    public void unregister()
+    {
+        HandlerList.unregisterAll(this);
+    }
 }
